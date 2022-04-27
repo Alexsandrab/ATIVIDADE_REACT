7e5c63a133a7ad079e966803307b9b00690f61ec
BrunoFranzon <bruno@madhat.de>
2022-04-27 15:55:54 +0200

    material ui

--- src/App.js
+++ src/App.js
@@ -4,9 +4,29 @@ import useFetchPeople from "../src/hooks/use-fetch-people";
 import { useState } from "react";
 import { BrowserRouter, Route, Switch, Link } from "react-router-dom";
 import Details from "./components/person-details";
-function App() {
-  const { isLoading, error, data, isFetching } = useFetchPeople();
+import { Button, makeStyles, Paper, Typography } from "@material-ui/core";
 
+const useStyles = makeStyles({
+  buttons: {
+    backgroundColor: "black",
+    height: 80,
+    marginRight: 4,
+  },
+  buttonsInactive: {
+    backgroundColor: "grey",
+    height: 80,
+    marginRight: 4,
+  },
+  textButton: {
+    color: "white",
+    fontWeight: "bold",
+  },
+});
+
+function App() {
+  const classes = useStyles();
+
+  const { isLoading, error, data, isFetching } = useFetchPeople();
   const [numeroElements, setNumeroElements] = useState(5);
   const [oldNumeroElements, setOldNumeroElements] = useState(0);
 
@@ -25,7 +45,7 @@ function App() {
       <div
         style={{
           display: "flex",
-          flexDirection: 'column',
+          flexDirection: "column",
           justifyContent: "center",
           alignItems: "center",
           marginTop: "25%",
@@ -41,6 +61,61 @@ function App() {
 
   if (error) return "An error has occurred: " + error.message;
 
+  const PessoaCard = () => {
+    return (
+      <div className="personListContainer">
+        <Paper className="personList" style={{ width: "700px" }}>
+          <div>
+            {/* Card de cada pessoa */}
+            <div>
+              {data?.results
+                ?.slice(oldNumeroElements, numeroElements)
+                .map((person) => (
+                  <Link to={person.name} className="links">
+                    <div className="personCard">
+                      Person: <span>{person.name}</span>
+                      <p>
+                        Ano de nasc.: <span>{person.birth_year}</span>{" "}
+                      </p>
+                    </div>
+                  </Link>
+                ))}
+            </div>
+
+            {/* Botoes */}
+            <div>
+              <Button
+                className={
+                  oldNumeroElements === 0
+                    ? classes.buttonsInactive
+                    : classes.buttons
+                }
+                onClick={() => previousButton()}
+                disabled={oldNumeroElements === 0 ? true : false}
+              >
+                <Typography className={classes.textButton}>Anterior</Typography>
+              </Button>
+
+              <Button
+                className={
+                  data?.results.length && oldNumeroElements !== 0
+                    ? classes.buttonsInactive
+                    : classes.buttons
+                }
+                onClick={() => forwardButton()}
+                disabled={
+                  numeroElements === data?.results.length ? true : false
+                }
+              >
+                <Typography className={classes.textButton}>Próximo</Typography>
+              </Button>
+            </div>
+          </div>
+        </Paper>
+      </div>
+    );
+  };
+
   return (
     <BrowserRouter>
       <Switch>
@@ -49,41 +124,7 @@ function App() {
         </Route>
 
         <Route path="/" exact>
-          <div className="personListContainer">
-            <div className="personList">
-              <div>
-                {data?.results
-                  ?.slice(oldNumeroElements, numeroElements)
-                  .map((person) => (
-                    <Link to={person.name} className="links">
-                      <div className="personCard">
-                        Person: <span>{person.name}</span>
-                        <p>
-                          Ano de nasc.: <span>{person.birth_year}</span>{" "}
-                        </p>
-                      </div>
-                    </Link>
-                  ))}
-              </div>
-
-              <div className="buttons">
-                <button
-                  onClick={() => previousButton()}
-                  disabled={oldNumeroElements === 0 ? true : false}
-                >
-                  Anterior
-                </button>
-                <button
-                  onClick={() => forwardButton()}
-                  disabled={
-                    numeroElements === data?.results.length ? true : false
-                  }
-                >
-                  Próximo
-                </button>
-              </div>
-            </div>
-          </div>
+          <PessoaCard />
         </Route>
       </Switch>
     </BrowserRouter>
